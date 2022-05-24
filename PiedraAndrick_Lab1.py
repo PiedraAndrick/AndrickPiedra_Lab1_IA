b3b32b193ca734ed88ed013eb9e278b9f84091d4
PiedraAndrick <65957499+PiedraAndrick@users.noreply.github.com>
2022-05-24 18:13:58 -0500

    Add files via upload

--- PiedraAndrick_Lab1.py
+++ PiedraAndrick_Lab1.py
@@ -4,4 +4,12 @@ Created on Tue May 24 18:08:36 2022
 
 @author: piedra
 """
-
+def vacuum_world():
+#Costo inicial
+    costo = 0
+    
+    #Definimos los estados iniciales de las habiaciones
+    estado_habitacion1 = input("Estado actual de la Habitacion 1 : ") #Ingreso del estado de la primera habitacion
+    estado_habitacion2 = input("Estado actual de la Habitacion 2 : ") #Ingreso del estado de la segunda habitacion
+    estado_habitacion3 = input("Estado actual de la Habitacion 3 : ") #Ingreso del estado de la tercera habitacion
+    vacuum_world()
